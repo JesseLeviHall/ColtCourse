9f87c2de3f965a0874f7067401fa6da3cddef638
jesse h <jesselevihall@gmail.com>
2023-03-20 15:48:22 -0600

    function isLeapYear

--- functionsExercise.ts
+++ functionsExercise.ts
@@ -25,3 +25,7 @@
 const twoFer = (name: string = "you"): string => {
   return `One for ${name}, one for me`;
 }
+
+const isLeapYear = (year: number): boolean => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
