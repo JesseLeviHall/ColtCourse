41c2372111f5a3f1ee766c7e7a6be46468ed4960
jesse h <jesselevihall@gmail.com>
2023-03-23 11:33:36 -0600

    Types and Interfaces

--- note.one.ts
+++ note.one.ts
@@ -2,7 +2,13 @@
 Colt Course Typescrypt
 
 Interfaces serve almost the same purpose as typea aliases with slightly different syntax. 
-Interfaces are used to define the structure of an object and object only. They create reusable, modular types that describe the shapes of objects. You can add to them in separate infterface of same name, and extend them into new interface that inherits parent interface properties. 
+Interfaces are used to define the structure of an object and object only. They create reusable, modular types that describe the shapes of objects. You can add to them in separate infterface of same name, and extend them into new interface that inherits parent interface properties, not limited to one interface parent. 
+
+//Describe differences between Types and Interfaces:
+ 1. Interfaces can be used to describe the structure of an object, while types can be used to describe the structure of a function, primitive, or union.
+ 2. Interfaces can be re-opened and extended, while types cannot.
+
+ Generally - gravitate towards interface when describing an object type. Otherwise use types. 
 
 */
 
