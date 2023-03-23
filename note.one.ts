d6cd41edda613c18f181637fbd952ef285cefa99
jesse h <jesselevihall@gmail.com>
2023-03-23 10:20:28 -0600

    modular types

--- note.one.ts
+++ note.one.ts
@@ -3,6 +3,8 @@ Colt Course Typescrypt
 
 Interfaces serve almost the same purpose as typea aliases with slightly different syntax. 
 
+Interfaces are used to define the structure of an object. They create reusable, modular types that describe the shapes of objects. 
+
 */
 
 
