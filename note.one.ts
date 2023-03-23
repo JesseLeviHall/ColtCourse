bc53327d058eb89918fdbfeb7676ffd913a8dda0
jesse h <jesselevihall@gmail.com>
2023-03-23 11:25:10 -0600

    inherits parent interface

--- note.one.ts
+++ note.one.ts
@@ -2,8 +2,7 @@
 Colt Course Typescrypt
 
 Interfaces serve almost the same purpose as typea aliases with slightly different syntax. 
-
-Interfaces are used to define the structure of an object. They create reusable, modular types that describe the shapes of objects. 
+Interfaces are used to define the structure of an object and object only. They create reusable, modular types that describe the shapes of objects. You can add to them in separate infterface of same name, and extend them into new interface that inherits parent interface properties. 
 
 */
 
