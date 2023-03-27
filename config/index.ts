2a7c21e9a11341e76aee4fb67e7971f3ea4a777d
jesse h <jesselevihall@gmail.com>
2023-03-26 20:39:15 -0600

    catch the days per week

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,13 @@
 interface Chicken {
   breed: string
-  layEggs: () => number
+  layEggs: (days: number) => number
   eggsPerWeek: number;
-}
-
-//a function that devides eggsPerWeek by 7
-const eggsPerDay = (eggLayer: Chicken) => {
-  return eggLayer.eggsPerWeek / 7;
 }
 
 const norma: Chicken = {
   breed: 'Jersey Giant',
   eggsPerWeek: 5,
-  layEggs: () => 5 / 7,
+  layEggs: (days: number) => (days / 7) * norma.eggsPerWeek,
 }
 
 const debra: Chicken = {
@@ -22,9 +17,9 @@ const debra: Chicken = {
 }
 
 //write a funciton that calls the layEggs method
-const lay = (eggLayer: Chicken) => {
-  eggLayer.layEggs();
+const lay = (eggLayer: Chicken, days: number) => {
+  eggLayer.layEggs(days);
 }
 
-lay(norma);
-lay(debra);
+lay(norma, 3);
+lay(debra, 5);
