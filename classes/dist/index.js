9ddf7d4a691316c60ac0696ffbebefbc7d6832fd
jesse h <jesselevihall@gmail.com>
2023-03-29 19:58:22 -0600

    player constructor

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,2 +1,16 @@
-console.log('Hello World!');
-console.log('YAAAA');
+class Player {
+	score = 0;
+	numLives = 10;
+	constructor(first, last) {
+		console.log('in the constructor');
+		this.first = first;
+		this.last = last;
+	}
+	taunt() {
+		console.log('I am the best!');
+	}
+}
+
+const Player1 = new Player('blue', 'steel');
+Player1.taunt();
+console.log(Player1);
