67340d4a2f85d33416f8e5642e8b118997d2475a
jesse h <jesselevihall@gmail.com>
2023-04-14 21:37:40 -0600

    get profit function

--- objectsExercise.ts
+++ objectsExercise.ts
@@ -50,3 +50,22 @@ function getProfit(movie: Movie): number {
 function destructureProfit({boxOffice: {grossWorldwide, budget}}: Movie): number {
   return grossWorldwide - budget;
 }
+
+// Write a function called getProfit that accepts a single Movie object
+
+// It should return the movie's worldwide gross minus its budget
+
+// For example...
+
+// getProfit(cats) => -21166652
+
+function getProfit(movie: Movie): number {
+  const {grossWorldwide, budget} = movie.boxOffice;
+  return grossWorldwide - budget;
+}
+
+function destructureProfit({boxOffice: {grossWorldwide, budget}}: Movie): number {
+  return grossWorldwide - budget;
+}
+
+// Write a function called getProfit that accepts a single Movie object
